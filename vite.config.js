88e493cdaf19c658440e8fa4df2bd6a8e58c02f7
Samir Roy <samirroy594@gmail.com>
2025-11-24 18:57:55 -0500

    base path added for in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+//https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
   },
-  base: "//se_project_react/",
+  base: "/se_project_react/",
 });
